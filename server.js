7b10e54a7ef155ca6b189c6d3ac38189d19e3cd0
RJH78 <35325011+rjh78@users.noreply.github.com>
2024-05-06 20:21:31 -0400

    add POST, PUT, and 2 GET API's

--- server.js
+++ server.js
@@ -5,11 +5,83 @@
 */
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 const PORT = 3000;
+const categoryArray = [
+  {
+    envId: 1,
+    envName: "Food-Market",
+    envBudget: 350,
+  },
+  {
+    envId: 2,
+    envName: "Rent",
+    envBudget: 1500,
+  },
+  {
+    envId: 3,
+    envName: "Auto-Gas",
+    envBudget: 80,
+  },
+];
+let envId = 1;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+//parses all incoming request data as JSON, i.e. strings
+app.use(bodyParser.json());
+
+//create a new envelope where the envelope name and dollar
+//amt are sent in the request body
+app.post("/envelopes", (req, res) => {
+  const { envName, envBudget } = req.body;
+  if (envName === "" || envBudget < 0) {
+    res.status(404).send("Invalid request body - no name or budget.");
+  }
+  const newCategory = { id: envId, name: envName, budget: envBudget };
+  envId++;
+  categoryArray.push(newCategory);
+  res.status(201).json(newCategory);
+  console.log(categoryArray);
+});
+
+//return all envelope categories created
+app.get("/envelopes", (req, res) => {
+  if (categoryArray.length > 0) {
+    res.status(200).json(categoryArray);
+  } else {
+    res.status(404).send("No categories created yet.");
+  }
+});
+
+//return a specific envelope where the envelope ID is
+//sent in the query parameter. req.params.envId is read
+//as a string, so convert to an integer for endpoint to work
+app.get("/envelopes/:envId", (req, res) => {
+  let searchId = Number(req.params.envId);
+  const found = categoryArray.find((element) => element.envId === searchId);
+  if (found) {
+    res.status(200).json(found);
+  } else {
+    res.status(404).send(`Envelope ID: ${searchId} not found.`);
+  }
+});
+
+app.put("/envelopes/:envId", (req, res) => {
+  let searchId = req.params.envId;
+  let newBudget = req.body.envBudget;
+  let newName = req.body.envName;
+  const found = categoryArray.find((element) => element.envId === searchId);
+  if (found) {
+    if (newBudget !== found.envBudget) {
+      found.envBudget = newBudget;
+    }
+    if (newName !== found.envName) {
+      found.envName = newName;
+    }
+    res.status(200).send("Updates made.");
+  } else {
+    res.status(404).send(`Envelope ID: ${searchId} not found.`);
+  }
 });
 
 app.listen(PORT, function () {
